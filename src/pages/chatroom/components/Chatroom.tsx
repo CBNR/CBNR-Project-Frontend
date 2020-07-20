7a7fef3b38c094a9571ea38e68107054291dc723
BlakeSzabo <igocrunk@gmail.com>
2020-07-21 02:30:15 +1000

    move store map in to chatbox

--- src/pages/chatroom/components/Chatroom.tsx
+++ src/pages/chatroom/components/Chatroom.tsx
@@ -3,26 +3,19 @@ import ChatroomHeader from "./ChatroomHeader";
 import { Container } from "@material-ui/core";
 import ChatroomInput from "./ChatroomInput";
 import Chatbox from "./Chatbox";
-import { StateDefinition } from "../../../store/reducer";
-import { connect } from "react-redux";
-import Message from "../../../models/message";
 
 interface ChatroomProps {
-    chatMessages: Message[];
 }
 
-const Chatroom: FC<ChatroomProps> = ({ chatMessages }) => {
+const Chatroom: FC<ChatroomProps> = () => {
     return (
         <Container>
             <ChatroomHeader title="CBNR" />
-            <Chatbox chatMessages={chatMessages} />
+            <Chatbox />
             <ChatroomInput />
         </Container>
     );
 }
 
-const mapStateToProps = (state: StateDefinition) => ({
-    chatMessages: state.chatMessages,
-});
 
-export default connect(mapStateToProps, null)(Chatroom);
+export default Chatroom
